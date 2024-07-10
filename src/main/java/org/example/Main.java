ef2c629934c722c402d4b7121088928d95c168c7
João Cláudio <leiloka46@gmail.com>
2024-07-10 18:00:31 -0300

    First class: Position

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.boardGame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Position position = new Position(3, 4);
+        System.out.println(position);
     }
 }
